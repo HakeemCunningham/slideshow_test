2a439e19a15ed02344b8cdbbcf382ba7c70b7b9a
Hakeem Cunningham <96095882+HakeemCunningham@users.noreply.github.com>
2024-05-21 14:01:15 -0800

    uploaded slideshow files

---
+++ slideshow.js
@@ -0,0 +1,35 @@
+// Thumbnail image controls
+function currentSlide(n) {
+      showSlides(slideIndex = n);
+    }
+ 
+ function showSlides(n) {
+     let i;
+     let slides = document.getElementsByClassName("slides");
+     let dots = document.getElementsByClassName("dot");
+     if (n > slides.length) {slideIndex = 1}
+     if (n < 1) {slideIndex = slides.length}
+     for (i = 0; i < slides.length; i++) {
+     slides[i].style.display = "none";
+     }
+     for (i = 0; i < dots.length; i++) {
+     dots[i].className = dots[i].className.replace(" active", "");
+     }
+     slides[slideIndex-1].style.display = "block";
+     dots[slideIndex-1].className += " active";
+    }
+ 
+ let slideIndex = 0;
+ showSlides();
+ 
+ function showSlides() {
+     let i;
+     let slides = document.getElementsByClassName("slides");
+  for (i = 0; i < slides.length; i++) {
+     slides[i].style.display = "none";
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}
+     slides[slideIndex-1].style.display = "block";
+  setTimeout(showSlides, 5000); // Change image every 5 seconds
+ }
